a6fe50080147a2ea959c0ed92b1763219f41050e
Tony Butler <tony-a.butler@ubs.com>
2016-04-25 16:01:42 +0100

    Initial Commit

---
+++ SwashbuckleDynamicObject/Controllers/FooBarController.cs
@@ -0,0 +1,21 @@
+﻿using System.Dynamic;
+using System.Web.Http;
+using Newtonsoft.Json;
+
+namespace SwashbuckleDynamicObject.Controllers
+{
+    // [JsonObject]
+    public class FooBar : DynamicObject
+    {
+        public string Foo { get; set; }
+        public int Bar { get; set; }
+    }
+
+    public class FooBarController : ApiController
+    {
+        public FooBar Get()
+        {
+            return new FooBar { Foo="Foo", Bar=123};
+        }
+    }
+}
